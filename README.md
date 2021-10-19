7c7d3bca3c873c331a67b81e603f24c58790f39c
Max Wofford <max@maxwofford.com>
2021-10-19 16:50:53 +0000

    Add mouse follower axis

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 ## Tag Ideas
 
-- magnetic poles
-- gravity
-- movement tags
-- logic tags
-  - ternary
-- magnifying tags
-- shadow text
-- blink
-- clickthrough
-- spin
-- footer
-- new page (not component, but a really good card for this)
-- header
-- audio
-- video
-- recorder & player
+- [ ] magnetic poles
+- [ ] gravity
+- [ ] movement tags
+- [ ] logic tags
+  - [ ] ternary
+- [ ] magnifying tags
+- [ ] shadow text
+- [ ] blink
+- [ ] clickthrough
+- [ ] spin
+- [ ] footer
+- [ ] new page (not component, but a really good card for this)
+- [ ] header
+- [ ] audio
+- [ ] video
+- [ ] recorder & player
+- [ ] mouse (cursor) changers
+- [x] mouse follower
+  - [x] follow X or Y axis only
--- public/follow-mouse/main.js
+++ public/follow-mouse/main.js
@@ -34,3 +34,75 @@ customElements.define('follow-mouse', class extends HTMLElement {
     })
   }
 })
+
+customElements.define('follow-mouse-x', class extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        #target {
+          position: fixed;
+        }
+
+        #target {
+          transform: translate(-50%, -50%);
+        }
+      </style>
+      <div id="target">
+      </div>
+    `
+
+    while(this.children.length > 0) {
+      this
+        .shadowRoot
+        .querySelector("#target")
+        .append(this.children[0])
+    }
+
+    document.body.addEventListener('mousemove', e => {
+      const target = this.shadowRoot.querySelector("#target");
+      target.style.left = `${e.clientX}px`;
+    })
+  }
+})
+
+customElements.define('follow-mouse-y', class extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        #target {
+          position: fixed;
+        }
+
+        #target {
+          transform: translate(-50%, -50%);
+        }
+      </style>
+      <div id="target">
+      </div>
+    `
+
+    while(this.children.length > 0) {
+      this
+        .shadowRoot
+        .querySelector("#target")
+        .append(this.children[0])
+    }
+
+    document.body.addEventListener('mousemove', e => {
+      const target = this.shadowRoot.querySelector("#target");
+      target.style.top = `${e.clientY}px`;
+    })
+  }
+})
